f0b783a5518511a918304e33c0ff05d2ba075852
mmertdogann <mm3rtdogann@gmail.com>
2021-04-04 16:26:02 +0300

    Add routes

--- application.py
+++ application.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return render_template("index.html", name=request.args.get("name", "world"))
+    return render_template("index.html")
+
+@app.route("/greet", methods=["POST"])
+def greet():
+    return render_template("greet.html", name=request.form.get("name", "world")) #request.args for GET requests
